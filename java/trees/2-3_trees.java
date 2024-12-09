1fd3da72798a742099170dbe48cb585c7996dd8c
Brian Msane <msanebrianboss@gmail.com>
2024-12-09 09:01:38 +0200

    cooking

---
+++ java/trees/2-3_trees.java
@@ -0,0 +1,7 @@
+class NodeFor2_3Tree<T> {
+    T firstData;
+    T secondData;
+    NodeFor2_3Tree middlePointer;
+    NodeFor2_3Tree leftPointer;
+    NodeFor2_3Tree rightPointer;
+}
--- java/trees/bst.java
+++ java/trees/bst.java
@@ -4,6 +4,7 @@ class Node <T> {
     private Node rightChild;
 
     void Node(T e){
+        // always insert a leaf node
         this.data = e;
         this.leftChild = null;
         this.rightChild = null;
@@ -30,7 +31,6 @@ class Node <T> {
     public void setData(T e){ data = e;}
     public void setLeftChild(Node left){ leftChild = left; }
     public void setRightChild(Node right){ rightChild = right; }
-
 }
 
 
@@ -66,16 +66,47 @@ public class Traversal{
 
 
     private void iterativeInOrder(Node root){
-        
+        Stack S = new Stack;
     }
     private void iterativePostOrder(Node root){
-        
+        Stack S = new Stack;
     }
     private void iterativePreOrder(Node root){
-        
+        // Push the root to the top of th stack
+        // while this stack is not empty
+            // pop and process the root
+            // if P has a right child
+                // for each node Y in teh left shell of the right child of P
+                    // push Y ont stack S
+        Stack S = new Stack;
+        S.push(this.root);
+        while(!(S.isEmpty())){
+            System.out.println(root.leftChild);
+            S.pop();
+            if(root.rightChild != null){
+                //
+            }
+        }
     }
+
     private void iterativeLevelOrder(Node root){
-        
+        //Enqueue the root into queue Q
+        //while Q is not empty (front and rear are not null)
+            // Dequeue the node at the front, process it
+            // If it had children,
+                // Enqueue all its children to Q
+        Queue Q = new Queue();
+        Q.Enqueue(root);
+        while(Q.front != null && Q.rear != null){
+            System.out.println(Q.Top());
+            Q.Dequeue();
+            if (root.leftChild != null){
+                Q.Enqueue(root.leftChild);
+                if (root.rightChild != null){
+                    Q.Enqueue(root.rightChild);
+                }
+            }
+        }
     }
 }
 
@@ -93,17 +124,13 @@ class binarySearchTree <T> {
         this.traverse(root.rightChild);
     }
 
-    public void delete(T e){
-        pos = this.find(e);
-    }
-
     public void find(T e){
         if (this.isEmpty())
             return null;
         else {
             if (this.root.data == e){
                 return this.root;
-            } else{
+            } else {
                 if (this.root.data > e){
                     this.rightChild.find(e);
                 } else if (this.root.data < e) {
@@ -114,14 +141,47 @@ class binarySearchTree <T> {
     }
 
     public void insert(T e){
-        if (this.isEmpty()){
-            Node p = new Node(e);
-        } else {
-            if (this.root.data > e){
-                this.root.rightChild.data = e;
+        if (e < this.data){
+            if (leftChild == null){
+                leftChild = new Node(e);
             } else {
-                this.root.leftChild.data = e;
+                leftChild.insert(e);
             }
+        } else if (e > this.data){
+            if (rightChild == null){
+                rightChild = new Node(e);
+            } else {
+                rightChild.insert(e)
+            }
+        }
+    }
+
+    public void delete(T e){
+        nodeE = this.find(e)
+        if (nodeE){
+            if (nodeE.isLeaf()){
+                nodeE = null;
+            } else {
+                // find in order successor
+                // promote it to this parent
+                inOrderSuccessor = this.findInOrderSuccessor();
+                nodeE.data  = inOrderSuccessor.data;
+            }
+        }
+    }
+
+    public Node findInOrderSuccessor(Node e){
+        // go right once and go left as far as you can
+        right = e.right;
+        while (right != null){
+            right  = right.leftChild;
+        }
+        return right;
+
+        // handle edge case
+            // if there is no right, take the next left value
+        if (e.rightChild == null){
+            return e.leftChild;
         }
     }
 
@@ -134,6 +194,6 @@ class binarySearchTree <T> {
     }
 
     public int depthOfNode(Node node){
-        
+        // same as the level of the node
     }
 }
